24b654a47736ee1437682ed026912f2a035e4dc3
H29id <h29id@proton.me>
2025-01-14 23:30:38 +0700

    Update deleteAccount route program

--- src/route/deleteAccount.js
+++ src/route/deleteAccount.js
@@ -1,76 +1,79 @@
 export default async(c, util, data, cookie) => {
-	const loggedIn = await util.loggedIn(c);
-	if (!loggedIn) return c.redirect("/");
+	try {
+		const loggedIn = await util.loggedIn(c);
+		if (!loggedIn) return c.redirect("/");
 
-	const sessionId = await cookie.get(c);
-	const session = await data.session.get(sessionId);
+		const sessionId = await cookie.get(c);
+		const session = await data.session.get(sessionId);
 
-	const accountId = session.account_id;
-	const account = await data.account.get(accountId);
+		const accountId = session.account_id;
+		const account = await data.account.get(accountId);
 
 
 
-	// delete posts and post images
-	const userPosts = await data.post.getAllByAccountId(accountId);
-	if (userPosts.length) {
-		const userPostIds = userPosts.map(x => x.post_id);
+		// delete posts and post images
+		const userPosts = await data.post.getAllByAccountId(accountId);
+		if (userPosts.length) {
+			const userPostIds = userPosts.map(x => x.post_id);
 
-		for (let postId of userPostIds) {
+			for (let postId of userPostIds) {
 
-			var postImages = await data.post.getImages(postId);
-			if (postImages.length) {
+				var postImages = await data.post.getImages(postId);
+				if (postImages.length) {
 
-				// delete image files and images data from database
-				for (let image of postImages) {
-					await util.deleteImageFromStorage("posts", image.file_name);
-					await data.post.deleteImages(image.post_id);
+					// delete image files and images data from database
+					var imagesFileName = postImages.map(x => x.file_name);
+					await util.deleteImageFromStorage("posts", imagesFileName);
+					await data.post.deleteImages(postId);
+
 				}
 
+				// delete post
+				await data.post.delete(postId);
+
 			}
-
-			// delete post
-			await data.post.delete(postId);
-
 		}
-	}
 
 
 
-	// delete reports and report images
-	const userReports = await data.post.getAllByAccountId(accountId);
-	if (userReports.length) {
-		const userReportIds = userReports.map(x => x.report_id);
+		// delete reports and report images
+		const userReports = await data.report.get.fetchAll(accountId);
+		if (userReports.length) {
+			const userReportIds = userReports.map(x => x.report_id);
 
-		for (let reportId of userReportIds) {
+			for (let reportId of userReportIds) {
 
-			var reportImages = await data.post.getImages(reportId);
-			if (reportImages.length) {
+				var reportImages = await data.report.getImages(reportId);
+				if (reportImages.length) {
 
-				// delete image files and images data from database
-				for (let image of reportImages) {
-					await util.deleteImageFromStorage("reports", image.file_name);
-					await data.post.deleteImages(image.post_id);
+					// delete image files and images data from database
+					var imagesFileName = reportImages.map(x => x.file_name);
+					await util.deleteImageFromStorage("reports", imagesFileName);
+					await data.report.deleteImages(reportId);
+
 				}
 
+				// delete report
+				console.log(reportId);
+				await data.report.delete(reportId);
+
 			}
-
-			// delete post
-			await data.report.delete(reportId);
-
 		}
+
+
+
+		// delete sessions
+		await data.session.deleteAll(accountId);
+
+		// delete account
+		await data.account.delete(accountId);
+
+		return c.html(await util.info(
+			"Akun Terhapus",
+			"Akun dan data yang terkait dengan akun telah dihapuskan.",
+			"/"
+		));
+	} catch(err) {
+		console.error(err);
 	}
-
-
-
-	// delete sessions
-	await data.session.deleteAll(accountId);
-
-	// delete account
-	await data.account.delete(accountId);
-
-	return await util.info(
-		"Akun Terhapus",
-		"Akun dan data yang terkait dengan akun telah dihapuskan.",
-		"/"
-	);
 }
